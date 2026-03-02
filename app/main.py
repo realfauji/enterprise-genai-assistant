b9dd77ebbc3ae1685bb37fc82fa4fdb50aac707b
Raj Bhardwaj <89062479+realfauji@users.noreply.github.com>
2026-03-02 20:17:40 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
-from app.db.session import engine
-from app.db.base import Base
-from app.models.user import User
-from app.models.chat_session import ChatSession
-from app.models.chat_message import ChatMessage
+# from app.db.session import engine
+# from app.db.base import Base
+# from app.models.user import User
+# from app.models.chat_session import ChatSession
+# from app.models.chat_message import ChatMessage
 from app.api.auth_routes import router as auth_router
 from app.api.chat_routes import router as chat_router
 from app.api.session_routes import router as session_router
 from fastapi.middleware.cors import CORSMiddleware
 # from contextlib import asynccontextmanager
-import app.db.init_db
+# import app.db.init_db
 
 
 # @asynccontextmanager
