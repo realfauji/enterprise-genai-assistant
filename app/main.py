05b73ef39dcf368d6293bf625c82330bde423c8e
Raj Bhardwaj <89062479+realfauji@users.noreply.github.com>
2026-03-05 11:02:13 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,25 +1,25 @@
 from fastapi import FastAPI
-# from app.db.session import engine
-# from app.db.base import Base
-# from app.models.user import User
-# from app.models.chat_session import ChatSession
-# from app.models.chat_message import ChatMessage
+from app.db.session import engine
+from app.db.base import Base
+from app.models.user import User
+from app.models.chat_session import ChatSession
+from app.models.chat_message import ChatMessage
 from app.api.auth_routes import router as auth_router
 from app.api.chat_routes import router as chat_router
 from app.api.session_routes import router as session_router
 from fastapi.middleware.cors import CORSMiddleware
-# from contextlib import asynccontextmanager
-# import app.db.init_db
+from contextlib import asynccontextmanager
+import app.db.init_db
 
 
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     async with engine.begin() as conn:
-#         await conn.run_sync(Base.metadata.create_all)
-#     yield
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
 
-# app = FastAPI(title="Enterprise GenAI Platform", lifespan=lifespan)
-app = FastAPI(title="Enterprise GenAI Platform")
+app = FastAPI(title="Enterprise GenAI Platform", lifespan=lifespan)
+# app = FastAPI(title="Enterprise GenAI Platform")
 
 app.add_middleware(
     CORSMiddleware,
